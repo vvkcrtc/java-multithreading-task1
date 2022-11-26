9af0576c58294740fbb6be3bfb905d411dafefb6
vvkcrtc <105951181+vvkcrtc@users.noreply.github.com>
2022-11-26 23:29:46 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        class ProcessText implements Runnable {
+            String text;
+
+            public ProcessText(String text) {
+                this.text = text;
+            }
+
+            void process() {
+                int maxSize = 0;
+
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            }
+
+            public void run() {
+                process();
+            }
+        }
+        long startTs = System.currentTimeMillis(); // start time
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 25; i++) {
+            threads.add(new Thread(new ProcessText(texts[i])));
+        }
+
+
+        for (Thread thread : threads) {
+            thread.start();
+            thread.join();
+        }
+
+
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
